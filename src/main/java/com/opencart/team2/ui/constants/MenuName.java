9963154eaba0e4291771655b58dbf342dcb11e04
Iryna <irysia198601@gmail.com>
2022-05-12 17:12:45 +0300

    add MenuPage, add MenuName

---
+++ src/main/java/com/opencart/team2/ui/constants/MenuName.java
@@ -0,0 +1,22 @@
+package com.opencart.team2.ui.constants;
+
+public enum MenuName {
+    DESKTOPS("desktops"),
+    LAPTOPSNOTEBOOKS("laptops&notebooks"),
+    COMPONENTS("components"),
+    TABLETS("tablets"),
+    SOFTWARE("software"),
+    PHONESPDAs("phones&pdas"),
+    CAMERAS("cameras"),
+    MP3PLAYERS("mp3players");
+
+    private final String name;
+
+    MenuName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/com/opencart/team2/ui/pages/AdmincurencyPage.java
@@ -0,0 +1,4 @@
+package com.opencart.team2.ui.pages;
+
+public class AdmincurencyPage {
+}
