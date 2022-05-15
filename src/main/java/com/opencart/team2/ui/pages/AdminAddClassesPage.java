38cc8254ba6e613596ac0e98bcb74bf3b2a008f7
LiliaB01 <lilia.baranovska@gmail.com>
2022-05-15 23:01:50 +0300

    AdminAddClassesPage

--- src/main/java/com/opencart/team2/ui/pages/AdminAddClassesPage.java
+++ src/main/java/com/opencart/team2/ui/pages/AdminAddClassesPage.java
@@ -2,67 +2,62 @@ package com.opencart.team2.ui.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class AdminAddClassesPage extends BasePage {
 
-    @FindBy (xpath = "//*[@id=\"content\"]/div[1]/div/ul/li[2]/a")
-    private WebElement Tax_Classes_Add;
-
     @FindBy (xpath = "//*[@id=\"content\"]/div[1]/div/div/button/i")
-    private WebElement Save_Add;
+    private WebElement saveAdd;
 
     @FindBy (xpath = "//*[@id=\"content\"]/div[1]/div/div/a")
-    private WebElement Cancel_Add;
+    private WebElement cancelAdd;
 
     @FindBy (xpath = "//*[@id=\"input-title\"]")
-    private WebElement Input_Title;
+    private WebElement inputTitle;
 
     @FindBy (xpath = "//*[@id=\"input-description\"]")
-    private WebElement Input_Description;
+    private WebElement inputDescription;
 
-    @FindBy (xpath = "//*[@id=\"tax-rule-row0\"]/td[1]/select")
-    private WebElement Tax_Rate_EcoTax;
+    @FindBy (xpath = "//*[@id=\"tax-rule-row0\"]/td[1]/select)")
+    private WebElement taxRate;
 
     @FindBy (xpath = "//*[@id=\"tax-rule-row0\"]/td[2]/select")
-    private WebElement Tax_Rate_BasedOn;
+    private WebElement taxBasedOn;
 
     @FindBy (xpath = "//*[@id=\"tax-rule-row0\"]/td[3]/input")
-    private WebElement Tax_Rate_InputPriority;
+    private WebElement taxRateInputPriority;
 
     @FindBy (xpath = "//*[@id=\"tax-rule\"]/tfoot/tr/td[2]/button")
-    private WebElement Add_Rate_Tax_Rates;
+    private WebElement addRateTaxRates;
+
+    @FindBy (xpath = "//*[@id=\"tax-rule-row0\"]/td[4]/button/i")
+    private WebElement removeRateTaxRates;
 
     public AdminAddClassesPage(WebDriver driver) {
         super(driver);
     }
 
-    public  WebElement Tax_Classes () {
-        return Tax_Classes_Add;
+    public WebElement getSaveAdd() {return saveAdd;}
+
+    public WebElement getCancel() {return cancelAdd;}
+
+    public WebElement getInputTitle() {
+        return inputTitle;
     }
 
-    public WebElement getSave() {
-        return Save_Add;
+    public WebElement getInputDescription() {
+        return inputDescription;
     }
 
-    public WebElement getCancel() {
-        return Cancel_Add;
+    public Select getTaxRate() {return new Select(taxRate);}
+
+    public Select getTaxBasedOn() {return new Select(taxBasedOn);}
+
+    public WebElement getTaxRateInputPriority() {return taxRateInputPriority;}
+
+    public WebElement getAddRateTaxRates() {
+        return addRateTaxRates;
     }
 
-    public WebElement getInput_Title() {
-        return Input_Title;
-    }
-
-    public WebElement getInput_Description() {
-        return Input_Description;
-    }
-
-    public WebElement getTax_Rate_EcoTax () {return Tax_Rate_EcoTax;}
-
-    public WebElement getTax_Rate_BasedOn() {return Tax_Rate_BasedOn;}
-
-    public WebElement getTax_Rate_InputPriority () {return Tax_Rate_InputPriority;}
-
-    public WebElement getAdd_Rate_Tax_Rates() {
-        return Add_Rate_Tax_Rates;
-    }
+    public WebElement getRemoveRateTaxRates() {return removeRateTaxRates;}
 }
