d7dd6b318636839b37a6d6a48c91a25d178c0378
Iryna <irysia198601@gmail.com>
2022-05-16 11:44:00 +0300

    Added CheckoutPage, MyAccountPage, CheckoutPageBL, MyAccountPageBL

---
+++ src/main/java/com/opencart/team2/ui/business/CheckoutBL.java
@@ -0,0 +1,65 @@
+package com.opencart.team2.ui.business;
+
+import com.opencart.team2.ui.pages.CheckoutPage;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutBL {
+    private CheckoutPage checkoutPage;
+
+    public CheckoutBL(WebDriver driver) {
+        checkoutPage = new CheckoutPage(driver);
+    }
+
+    public void firstNameButton() {
+        checkoutPage.getFirstNameButton().click();
+    }
+
+    public void lastNameButton() {
+        checkoutPage.getLastNameButton().click();
+    }
+
+    public void companyButton() {
+        checkoutPage.getCompanyButton().click();
+    }
+
+    public void address1Button() {
+        checkoutPage.getAddress1Button().click();
+    }
+
+    public void address2Button() {
+        checkoutPage.getAddress2Button().click();
+    }
+
+    public void cityButton() {
+        checkoutPage.getCityButton().click();
+    }
+
+    public void postCodeButton() {
+        checkoutPage.getPostCodeButton().click();
+    }
+
+    public void countryButton() {
+        checkoutPage.getCountryButton().click();
+    }
+
+    public void regionButton() {
+        checkoutPage.getRegionButton().click();
+    }
+
+    public void continueButton() {
+        checkoutPage.getContinueButton().click();
+    }
+
+    public void existingAddressButton(int id) {
+        checkoutPage.getExistingAddressButton(id).click();
+    }
+
+    public void eddCommentsButton() {
+        checkoutPage.getEddCommentsButton().click();
+    }
+
+    public void termsConditionsButton() {
+        checkoutPage.getTermsConditionsButton().click();
+    }
+
+}
